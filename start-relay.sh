4833da16457ed93aede50676fa347c0138324719
Ben Sanders <ben.a.sanders@gmail.com>
2021-11-22 10:59:49 -0800

    Update start-relay.sh

--- start-relay.sh
+++ start-relay.sh
@@ -11,13 +11,13 @@ if [[ $SOURCE_URL == rtsp://* ]] && [ "$FORCE_FFMPEG_SOURCE" == "false" ]; then
    echo "/proxy.yml"
    cat /proxy.yml
    echo "Starting rtsp proxy from $SOURCE_URL to rtsp://0.0.0.0:8554/$STREAM_NAME..."
-   rtsp-simple-proxy /proxy.yml
+   /bin/rtsp-simple-proxy /proxy.yml
 
 else
 
    if [ "$SOURCE_URL" != "" ]; then
       echo "Starting rtsp server..."
-      rtsp-simple-server &
+      /bin/rtsp-simple-server &
       sleep 2
 
       echo "Start relaying from $SOURCE_URL to rtsp://0.0.0.0:8554/$STREAM_NAME"
@@ -31,6 +31,6 @@ else
    else
       echo "Won't restream a source feed to the server because SOURCE_URL was not defined"
       echo "Starting rtsp server. You can publish feeds to it (ex.: ffmpeg -i somesource.mjpg -c copy -f rtsp rtsp://localhost:8554/myfeed)"
-      rtsp-simple-server
+      /bin/rtsp-simple-server
    fi
 fi
